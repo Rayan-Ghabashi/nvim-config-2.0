4708b86a71edcfdea75a2974120fabc2b6c71f52
Rayan <rayanag789@gmail.com>
2024-12-09 12:11:00 +0300

    modified:   lua/davinci/init.lua 	new file:   lua/davinci/lines.lua

--- lua/davinci/init.lua
+++ lua/davinci/init.lua
@@ -1,2 +1,3 @@
 require("davinci.remap")
+require("davinci.lines")
 print("hello from davinci")
---
+++ lua/davinci/lines.lua
@@ -0,0 +1,4 @@
+vim.opt.relativenumber = true
+-- Enable absolute line numbers for the current line
+vim.opt.number = true
+
